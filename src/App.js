1fd86af29650ee68b6254a26ca03e4df5cefeb61
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-01-24 19:54:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
->>>>>>> 74b3555cbe89cb756c81c66c9dc7fa6e41ed2783
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './components/Home';
@@ -11,59 +8,9 @@ import Services from './components/Services';
 import Schemes from './components/Schemes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-<<<<<<< HEAD
-const appRouter = createBrowserRouter([
-  {
-    path: '/',
-    element: (
-      <div>
-        <Navbar />
-        <Home />
-        <Footer />
-      </div>
-    ),
-  },
-  {
-    path: '/about',
-    element: (
-      <div>
-        <Navbar />
-        <About />
-        <Footer />
-      </div>
-    ),
-  },
-  {
-    path: '/services',
-    element: (
-      <div>
-        <Navbar />
-        <Services />
-        <Footer />
-      </div>
-    ),
-  },
-  {
-    path: '/schemes',
-    element: (
-      <div>
-        <Navbar />
-        <Schemes />
-        <Footer />
-      </div>
-    ),
-  },
-]);
-
 function App() {
   return (
-    <div>
-      <RouterProvider router={appRouter} />
-    </div>
-=======
-function App() {
-  return (
-    <Router>
+    <>
       <Navbar />
       <div className="container mt-4">
         <Routes>
@@ -74,8 +21,7 @@ function App() {
         </Routes>
       </div>
       <Footer />
-    </Router>
->>>>>>> 74b3555cbe89cb756c81c66c9dc7fa6e41ed2783
+    </>
   );
 }
 
